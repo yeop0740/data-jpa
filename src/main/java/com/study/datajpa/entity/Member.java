9eeb3c28acaa4187a060fdd8c86a4bb6ef1b0cfa
yeop0740 <ssucgoo@gmail.com>
2023-08-02 18:44:35 +0900

    프로젝트 생성 및 엔티티 구성

---
+++ src/main/java/com/study/datajpa/entity/Member.java
@@ -0,0 +1,20 @@
+package com.study.datajpa.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+}
---
+++ src/main/java/com/study/datajpa/entity/Team.java
@@ -0,0 +1,20 @@
+package com.study.datajpa.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+}
